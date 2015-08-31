47041826e817f7a9e4690fad7ef5a08ea598acc3
mwg219 <mwg219@lehigh.edu>
2015-08-31 18:51:04 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,30 @@
+////////////////////////////////////////////////////////////////////////////////
+//Will Gilbert
+//Demo 1
+// Welcome Class Java Program
+//
+//  first compile the program
+//      javac WelcomeClass.java
+//  run the program
+//      java WelcomeClass//
+
+//  define a class
+public class WelcomeClass{
+    
+//  add main method
+  public static void main(String[] args) {
+
+//  print the statement with box around it
+    System.out.println("    -----------     ");
+    System.out.println("    | WELCOME |     ");
+    System.out.println("    -----------     ");
+    System.out.println("  ^  ^  ^  ^  ^  ^  ");
+    System.out.println(" | || || || || || | ");
+    System.out.println("|-M--W--G--2--1--9-|");
+    System.out.println(" | || || || || || | ");
+    System.out.println("  v  v  v  v  v  v  ");
+    System.out.println("My name is Will Gilbert and I was born in Washington DC, but I lived in Puerto Rico for 10 years and then I moved to Detroit, MI.");
+  
+    //test/testtest
+  }    
+}
